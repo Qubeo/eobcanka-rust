93bd2b65ae469f938e3b24ae703600135189bf2f
Qubeo <32247672+Qubeo@users.noreply.github.com>
2019-01-20 17:33:24 +0100

    fleshing out MyCard

--- src/mycard.rs
+++ src/mycard.rs
@@ -116,6 +116,7 @@ impl MyCard {
     }
 
     pub fn get_data(&self, tag_id: u8, auth_id: u8) -> Vec<u8> {
+
         let auth_id = auth_id << 4;
         let mut request = vec![0x80, 0xCA, (auth_id | 1), (auth_id | tag_id), 0];
 
@@ -128,9 +129,12 @@ impl MyCard {
         let mut response_vec = response.unwrap().to_vec();
 
         if self.get_SW(&response_vec) == [0x90, 0x00] {
+
             println!("Got 0x90, 0x00 right in get_SW!");
             return response_vec;
+
         } else if self.get_SW1(&response_vec) == 0x6C {
+
             println!("Got 0x6C in SW1!");
             let request_len = request.len();
             request[request_len - 1] = self.get_SW2(&response_vec); // Q: Isn't this a nonsense? Shouldn't reverse the array?
@@ -166,42 +170,26 @@ impl MyCard {
 // pub struct ICardInterface { }
 
 impl MyCard {
-    /*
-    pub fn new() {
-        MyCard {
-        }
+
+    // pub fn select_application(app_id: [u8; 8]) -> bool { }
+
+    pub fn select_application(&self, app_id: Vec<u8>) -> bool {
+
+        let cmd_select_applet_mgmt = [0x00, 0xA4, 0x04, 0x0C, 0x09, 0xD2, 0x03, 0x10, 0x01, 0x00, 0x01, 0x00, 0x02, 0x02];        
+        let mut result_apdu_buf = [0; MAX_BUFFER_SIZE];
+        
+        // let result_apdu = card.transmit(&cmd_select_applet_mgmt, &mut result_apdu_buf);
+        let result_apdu = self.my_card.transmit(&cmd_select_applet_mgmt, &mut result_apdu_buf);
+
+        return true;
     }
-    */
 
-    // pub fn select_application(app_id: [u8; 8]) -> bool {
-
-    /* public boolean selectApplication(byte[] appId) throws CardException {
-        if (currentApplication != null) {
-            if (Arrays.equals(currentApplication, appId)) { //Don't select application if is already set
-                return true;
-            }
-        }
-
-        byte[] selectApplet = new byte[]{
-                0x00, (byte) 0xA4, 0x04, 0x0C, (byte) appId.length,
-        };
-        IResponseAPDU r = c.transmit(c.createCommand((HexUtils.concatArrays(selectApplet, appId))));
-
-        if (r.getSW() == 0x9000) {
-            currentApplication = appId;
-            return true;
-        }
-        return false;
-    }
-    */
-
-    // Q: Why this isn't a const? Do the different app_id lenths differ?
-    // let cmd_select_applet = [0x00, 0xA4, 0x0C, app_id.len() as u8];
-    //let response_apdu = self.card.transmit(self.card.createCommand((HexUtils.concatArrays(select_applet_cmd, appId))));
-
-    pub fn transmit(command: Vec<u8>) {
-        // TODO: Parameter to the ICommandAPDU type
-
+    pub fn get_card_number(&self) -> Vec<u8> {
+        
+        self.select_application(MyCard::APP_ID_CARD_MANAGEMENT.to_vec());        
+        let card_number_request = self.get_data(MyCard::TAG_ID_CARD_NUMBER, 0);
+        
+        card_number_request.to_vec()
     }
 
     pub fn create_command(command_data: Vec<u8>) {
@@ -209,43 +197,8 @@ impl MyCard {
 
     }
 
-    pub fn get_request_command() -> Vec<u8> {
-        vec![0x00]
-    }
-
     pub fn get_ATR() -> Vec<u8> {
         // TODO: How long array?
         vec![0x00]
-    }
-
-    pub fn getData02(&self, tag_id: u8, auth_id: u8) -> Vec<u8> {
-        /*
-        let mut authId = authId << 4;
-        let request_command = [
-            0x80,
-            0xCA,
-            (authId | 1) as u8,
-            (authId | tagId) as u8,
-            0
-        ];
-
-        let response_data = self.transmit(self.create_command(request_command));
-
-        return response_data;
-        */
-
-        return vec![0x00];
-
-        // TODO: Put the 0x9000 result in a legible constant like RESULT_OK
-        /* if (r.getSW() == 0x9000) {
-            return r.getData();
-        } else if (r.getSW1() == 0x6c) {
-            request[request.length - 1] = (byte) r.getSW2();
-            r = c.transmit(c.createCommand(request));
-            if (r.getSW() == 0x9000) {
-                return r.getData();
-            }
-        }
-        return Err("Error"); */
     }
 }
